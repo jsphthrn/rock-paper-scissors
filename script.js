0d049063c9e657de98bf1a7692aa1a29bd818dd4
José Yáñez <jlyp_sn@protonmail.com>
2025-02-19 20:22:02 -0600

    Finish winning logic.

--- script.js
+++ script.js
@@ -23,18 +23,31 @@ function playRound (humanChoice, computerChoice) {
         if (computerChoice === "scissors") {
             computerScore = computerScore + 1;
             console.log("You lost this round! Scissors beat paper.");
+            return 0;
         } else if (computerChoice === "rock") {
             humanScore = humanScore + 1;
             console.log("You won this round! Paper beats rock.");
+            return 0;
         }
     } else if (humanChoice === "rock") {
         if (computerChoice === "scissors") {
             humanScore = humanScore + 1;
             console.log("You won this round! Rock beats scissors.");
+            return 0;
         } else if (computerChoice === "paper") {
             computerScore = computerScore + 1;
             console.log("You lost this round! Paper beats rock.");
-
+            return 0;
+        }
+    } else if (humanChoice === "scissors") {
+        if (computerChoice === "paper") {
+            humanScore = humanScore + 1;
+            console.log("You won this round! Scissors beats paper.");
+            return 0;
+        } else if (computerChoice === "rock") {
+            computerScore = computerScore + 1;
+            console.log("You lost this round! Rock beats scissors.");
+            return 0;
         }
     }
 
